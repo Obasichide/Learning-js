f75ae5f7ed06b3bca58ee24102a056c6097ec7e7
Obasichide <ichidefavour2022@gmail.com>
2026-09-23 16:32:11 +0100

    Initial commit

---
+++ first day.js
@@ -0,0 +1,8 @@
+console.log("Javascript is working");
+const name='Ichide Oborakpororo';
+const education='Computer Science With Economics';
+const skill='Backend Developer';
+const button= document.querySelector(".Profilebutton");
+button.addEventListener("click",() => {
+    alert("Welcome beautiful");
+});
